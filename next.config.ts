05e189430af72f10e913971ed8ff21c2e1fd8abb
akira <84250791+akira0816@users.noreply.github.com>
2025-03-24 04:34:48 +0900

    03243

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
+  eslint: {
+    // eslintのlint checkをbuild時にoff
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // type checkをbuild時にoff
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
